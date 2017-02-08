01ed7800999f7890ef369a84915b526184938a93
Arseniy Ivanov <freeatnet@freeatnet.com>
2017-02-08 15:57:33 -0500

    Automatically commit new scrapes

--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@ ACADEM_SESSION='201701'
 SECTION='S,Y'
 
 [ -d $ACADEM_SESSION ] || mkdir $ACADEM_SESSION
+
+git pull --rebase
 curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json
+
+git add "${ACADEM_SESSION}/"
+git commit -m "Add new scrape"
+git push origin master
+
