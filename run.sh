4526d019c61bcfbd220a4f6723e4422e964fc86b
ivan <ivanzhangsolutions@gmail.com>
2017-02-28 16:14:08 -0500

    revert temporarily

--- run.sh
+++ run.sh
@@ -5,12 +5,7 @@ SECTION='S,Y'
 [ -d $ACADEM_SESSION ] || mkdir $ACADEM_SESSION
 
 git pull --rebase
-
-SCRAPENAME=$(date +%s).json
-
-curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$SCRAPENAME 
-
-cp $ACADEM_SESSION/$SCRAPENAME $ACADEM_SESSION/latest.json
+curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json
 
 git add "${ACADEM_SESSION}/"
 git commit -m "Add new scrape"
