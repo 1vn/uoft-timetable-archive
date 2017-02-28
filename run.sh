c4eea52b5f0b62e38303c50b44aae3ecc3f72a1e
ivan <ivanzhangsolutions@gmail.com>
2017-02-28 15:56:20 -0500

    i am bash noob

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ SECTION='S,Y'
 
 git pull --rebase
 
-$scrape_file_name=$(date +%s).json
+SCRAPENAME=$(date +%s).json
 
-curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$scrape_file_name 
+curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$SCRAPENAME 
 
-cp $ACADEM_SESSION/$scrape_file_name $ACADEM_SESSION/latest.json
+cp $ACADEM_SESSION/$SCRAPENAME $ACADEM_SESSION/latest.json
 
 git add "${ACADEM_SESSION}/"
 git commit -m "Add new scrape"
