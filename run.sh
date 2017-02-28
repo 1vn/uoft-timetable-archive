ef05cedfa9d589112fa385c3d959c99532288211
ivan <ivanzhangsolutions@gmail.com>
2017-02-28 15:53:56 -0500

    make name into variable

--- run.sh
+++ run.sh
@@ -5,9 +5,12 @@ SECTION='S,Y'
 [ -d $ACADEM_SESSION ] || mkdir $ACADEM_SESSION
 
 git pull --rebase
-curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json 
 
-cp $ACADEM_SESSION/$(date +%s).json $ACADEM_SESSION/latest.json
+$scrape_file_name=$(date +%s).json
+
+curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/scrape_file_name 
+
+cp $ACADEM_SESSION/$scrape_file_name $ACADEM_SESSION/latest.json
 
 git add "${ACADEM_SESSION}/"
 git commit -m "Add new scrape"
