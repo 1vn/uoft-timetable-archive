8a85bea0c80b3430f618b9fa3a61aa7d53198369
ivan <ivanzhangsolutions@gmail.com>
2017-02-28 15:49:56 -0500

    make latest.json file

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ SECTION='S,Y'
 [ -d $ACADEM_SESSION ] || mkdir $ACADEM_SESSION
 
 git pull --rebase
-curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json
+curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json 
+
+cp $ACADEM_SESSION/$(date +%s).json $ACADEM_SESSION/latest.json
 
 git add "${ACADEM_SESSION}/"
 git commit -m "Add new scrape"
